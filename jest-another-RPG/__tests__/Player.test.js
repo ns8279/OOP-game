fb11c49cc0e1ea059e61c76807fe40de08276205
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-05-13 09:16:49 -0700

    finishing the player obejct constructor

--- jest-another-RPG/__tests__/Player.test.js
+++ jest-another-RPG/__tests__/Player.test.js
@@ -36,4 +36,68 @@ test("gets inventory from player or return false", ()=> {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
+});
+
+//test to get information about player's health
+test('gets player health value', () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+//test to check the Player Health
+test('check if the player is alive or not', () =>{
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+//test to reduce player health
+test('subtracts from Player health', () => {
+    const player = new Player ('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
+
+//test to get attack value
+test('to get players attack value', () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+    
+})
+
+//test to add potion to the inventory
+test('adds a potion to the inventory of player', ()=> {
+    const player =new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+
+});
+
+//test to use a potion from the inventory
+test('uses a potion from the inventory', () => {
+    const player =new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
 })
--- jest-another-RPG/lib/Player.js
+++ jest-another-RPG/lib/Player.js
@@ -28,6 +28,62 @@ Player.prototype.getInventory = function () {
     return false;
 };
 
+//Method to get player health data
+Player.prototype.getHealth = function() {
+    return ` ${this.name}'s health is now ${this.health}!`;
+};
+
+//Method to check if the Player is alive
+Player.prototype.isAlive = function() {
+    if(this.health === 0){
+        return false;
+    }
+    return true;
+};
+
+//Method to subtract player health
+Player.prototype.reduceHealth = function(health) {
+    this.health -= health;
+
+    if(this.health < 0) {
+        this.health = 0;
+    }
+
+}
+
+//Method to get Attack value
+Player.prototype.getAttackValue = function() {
+    const min = this.strength - 5;
+    const max = this.strength + 5;
+
+    return Math.floor(Math.random() * (max-min) + min);
+};
+
+//Method to add Potion into the player inventory
+Player.prototype.addPotion = function(potion) {
+    this.inventory.push(potion);
+}
+
+//Method to use Potion
+Player.prototype.usePotion = function(index) {
+    const potion = this.getInventory().splice(index, 1)[0]; //splice method removes item from an array and returns the removed item as new array
+
+    switch (potion.name) {
+        case 'agility':
+            this.agility += potion.value;
+            break;
+        case 'health':
+            this.health += potion.value;
+            break;
+        case 'strength':
+            this.strength += potion.value;
+            break;
+    };
+};
+
+
+
+
 //export the Player Constructor
 module.exports = Player;
 
